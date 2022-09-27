7f2b417292c3b796a4cc4df4038c6963e20fe8c0
p3rsidh <persidh.pimenta@gmail.com>
2022-09-27 10:49:07 -0300

    Adicionando Spring

---
+++ src/main/java/com/Recirende/Fidelidade/FidelidadeApplication.java
@@ -0,0 +1,13 @@
+package com.Recirende.Fidelidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FidelidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FidelidadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Recirende/Fidelidade/FidelidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Recirende.Fidelidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FidelidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
