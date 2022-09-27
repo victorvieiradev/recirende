a29aaa512a8b203b8507ab78991115424deebbc5
victorvieiradev <contatovictorvieira@outlook.com>
2022-09-27 16:59:52 -0300

    Finalizado os tests da classe entity embalagem

---
+++ src/test/java/com/Recirende/Fidelidade/models/EmbalagemModelTests.java
@@ -0,0 +1,21 @@
+package com.Recirende.Fidelidade.models;
+
+import com.Recirende.Fidelidade.Model.EmbalagemModel;
+import com.Recirende.Fidelidade.Model.enuns.EstadoEnum;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EmbalagemModelTests {
+    @Test
+    public void embalagemModelTests(){
+        EmbalagemModel embalagem = new EmbalagemModel();
+        embalagem.setNumeroDeSerie("123d");
+        Assertions.assertEquals("123d", embalagem.getNumeroDeSerie());
+        embalagem.setId(1L);
+        Assertions.assertEquals(1L, embalagem.getId());
+        embalagem.setMarca("doritos");
+        Assertions.assertEquals("doritos", embalagem.getMarca());
+        embalagem.setLocalDeColeta(EstadoEnum.DF);
+        Assertions.assertEquals(EstadoEnum.DF, embalagem.getLocalDeColeta());
+    }
+}
