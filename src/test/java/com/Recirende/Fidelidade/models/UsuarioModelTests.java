323df223a84059f9718443fbfc4c916884351215
victorvieiradev <contatovictorvieira@outlook.com>
2022-09-27 16:46:36 -0300

    finalizado o test da entity usuario

--- src/test/java/com/Recirende/Fidelidade/models/UsuarioModelTests.java
+++ src/test/java/com/Recirende/Fidelidade/models/UsuarioModelTests.java
@@ -8,12 +8,10 @@ import org.junit.jupiter.api.Test;
 public class UsuarioModelTests {
     @Test
     public void usuarioModelTests(){
-    EmbalagemModel
         UsuarioModel usuario = new UsuarioModel();
         usuario.setNome("Ana");
         usuario.setCpf(12312343221L);
         usuario.setPontos(1500L);
-        usuario.setEmbalagemModel();
         Assertions.assertEquals("Ana", usuario.getNome());
         Assertions.assertEquals(12312343221L, usuario.getCpf());
         Assertions.assertEquals(1500L, usuario.getPontos());
